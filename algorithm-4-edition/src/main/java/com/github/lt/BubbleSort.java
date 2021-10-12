34f2758868673c429745ce460cf8ce419b9316a6
litong <cd.litong@icloud.com>
2021-10-12 11:48:42 +0800

    冒泡排序

---
+++ algorithm-4-edition/src/main/java/com/github/lt/BubbleSort.java
@@ -0,0 +1,35 @@
+package com.github.lt;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+
+
+    public static void bubbleSort(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        int length = arr.length;
+        int wall = 0;
+        int point = length - 1;
+        int temp = 0;
+        while (wall < length) {
+            while (point > wall) {
+                if (arr[point] < arr[point - 1]) {
+                    temp = arr[point];
+                    arr[point] = arr[point - 1];
+                    arr[point - 1] = temp;
+                }
+                point--;
+            }
+            point = length - 1;
+            wall++;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] testArr = {23, 78, 45, 8, 32, 56};
+        bubbleSort(testArr);
+        System.out.println(Arrays.toString(testArr));
+    }
+}
