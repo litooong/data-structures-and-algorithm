2bd12802548df06f8f2579aed06c082464df5b0e
litong <cd.litong@icloud.com>
2022-08-14 14:07:14 +0800

    调整项目结构，新增日常练习模块

---
+++ daily-practice/src/main/java/litooong/com/github/datastructures/SuffixExpression.java
@@ -0,0 +1,12 @@
+package litooong.com.github.datastructures;
+
+/**
+ * @Description 使用栈来将中缀表达式转换为后缀表达式
+ *
+ * eg:
+ * 中缀：9 + (3 - 1) * 3 + 10 / 2
+ * 后缀：9 3 1 - 3 * + 10 2 / +
+ */
+public class SuffixExpression {
+
+}
